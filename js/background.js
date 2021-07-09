bbbaa7fa58841acba3194277e3db28a7449228e5
hhw12409 <sksk2425@naver.com>
2021-07-09 16:17:58 +0900

    Vanilla js

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)]
+
+const bgImage = document.createElement("img")
+const bgimg = document.getElementById("to-do_body")
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
+
